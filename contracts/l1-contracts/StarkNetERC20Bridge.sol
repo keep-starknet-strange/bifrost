4ed1673fccbd729e56b73bd211554cb893ef8e0d
amanusk <amanusk@pm.me>
2023-01-02 13:19:53 +0200

    Split bridge utils to library

--- contracts/l1-contracts/StarkNetERC20Bridge.sol
+++ contracts/l1-contracts/StarkNetERC20Bridge.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Context.sol";
 
+import "hardhat/console.sol";
+
 import "./BridgedERC20.sol";
+import "./BridgeUtils.sol";
 
 /**
  * Responsible for deploying new ERC20 contracts via CREATE2
@@ -24,8 +27,9 @@ contract StarkNetERC20Bridge is Context {
      */
     constructor() {
         baseAccount = address(new BridgedERC20{ salt: keccak256("V0.1") }());
-        BridgedERC20(baseAccount).init(address(this), "STAB", "STAB");
-        // This should be either configurable or changable by some address
+        // This is the contract that will be cloned to all others
+        // BridgedERC20(baseAccount).init(address(this), "STAB", "STAB");
+        // This should be either configurable or changeable by some address
         starknetCore = IStarknetCore(0xde29d060D45901Fb19ED6C6e959EB22d8626708e);
     }
 
@@ -36,10 +40,10 @@ contract StarkNetERC20Bridge is Context {
         string memory name,
         string memory symbol
     ) public returns (address) {
-        // bytes32 salt = keccak256(abi.encodePacked(_deployer));
+        // The salt is uniquely identified by the L2 contract address
+        // TODO: consider chainID
         bytes32 salt = bytes32(tokenL2Address);
         address payable clone = createCloneCreate2(salt);
-        // Todo, replace deployer with the factory address
         BridgedERC20(clone).init(_deployer, name, symbol);
         return clone;
     }
@@ -50,6 +54,7 @@ contract StarkNetERC20Bridge is Context {
      * @param _salt Salt for CREATE2
      */
     function createCloneCreate2(bytes32 _salt) internal returns (address payable result) {
+        console.log("Create base account", baseAccount);
         bytes20 targetBytes = bytes20(baseAccount);
         assembly {
             let clone := mload(0x40)
@@ -64,8 +69,9 @@ contract StarkNetERC20Bridge is Context {
     function createL1Instance(uint256 tokenL2Address, string calldata tokenName, string calldata tokenSymbol) public {
         uint256[] memory payload = new uint256[](2);
         // tokenName and Symbol are upto 32 bytes
-        payload[0] = strToUint(tokenName);
-        payload[1] = strToUint(tokenSymbol);
+        // They need to be converted to Uint as this is how they are sent via SN messaging
+        payload[0] = BridgeUtils.strToUint(tokenName);
+        payload[1] = BridgeUtils.strToUint(tokenSymbol);
         // Consume the message from the StarkNet core contract.
         // This will revert the (Ethereum) transaction if the message does not exist.
         starknetCore.consumeMessageFromL2(tokenL2Address, payload);
@@ -86,8 +92,8 @@ contract StarkNetERC20Bridge is Context {
         address createdToken = createERC20(
             address(this),
             tokenL2Address,
-            uintToString(tokenName),
-            uintToString(tokenSymbol)
+            BridgeUtils.uintToString(tokenName),
+            BridgeUtils.uintToString(tokenSymbol)
         );
         l2Addresses[createdToken] = tokenL2Address;
         l1Addresses[tokenL2Address] = createdToken;
@@ -97,7 +103,7 @@ contract StarkNetERC20Bridge is Context {
         require(l1Addresses[tokenL2Address] != address(0), "No L1 token initiated for this l2 address");
         uint256[] memory payload = new uint256[](3);
         payload[0] = tokenReceiver;
-        (payload[1], payload[2]) = toSplitUint(tokenAmount);
+        (payload[1], payload[2]) = BridgeUtils.toSplitUint(tokenAmount);
         // Consume the message from the StarkNet core contract.
         // This will revert the (Ethereum) transaction if the message does not exist.
         starknetCore.consumeMessageFromL2(tokenL2Address, payload);
@@ -115,7 +121,7 @@ contract StarkNetERC20Bridge is Context {
         require(l1Addresses[tokenL2Address] != address(0), "No L1 token initiated for this l2 address");
         uint256[] memory payload = new uint256[](3);
         payload[0] = l2Recipient;
-        (payload[1], payload[2]) = toSplitUint(amount);
+        (payload[1], payload[2]) = BridgeUtils.toSplitUint(amount);
 
         starknetCore.sendMessageToL2(tokenL2Address, BRIDGE_FROM_L1_SELECTOR, payload);
 
@@ -125,44 +131,5 @@ contract StarkNetERC20Bridge is Context {
         // If we remove storage, transaction will fail here after not finding ERC20
         bool success = IERC20(tokenL1Address).burn(_msgSender(), amount);
         require(success, "Burning on L1 Failed");
-    }
-
-    function uintToString(uint256 v) public pure returns (string memory str) {
-        return Strings.toString(v);
-    }
-
-    function strToUint(string memory text) public pure returns (uint256 res) {
-        bytes32 stringInBytes32 = bytes32(bytes(text));
-        uint256 strLen = bytes(text).length; // TODO: cannot be above 32
-        require(strLen <= 32, "String cannot be longer than 32");
-
-        uint256 shift = 256 - 8 * strLen;
-
-        uint256 stringInUint256;
-        assembly {
-            stringInUint256 := shr(shift, stringInBytes32)
-        }
-        return stringInUint256;
-    }
-
-    function computeAddress(uint256 tokenL2Address) public view returns (address tokenL1Address) {
-        bytes20 targetBytes = bytes20(baseAccount);
-        bytes32 baseCodeHash;
-        assembly {
-            let clone := mload(0x40)
-            mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
-            mstore(add(clone, 0x14), targetBytes)
-            mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
-            baseCodeHash := keccak256(clone, 0x37)
-        }
-        tokenL1Address = address(
-            uint160(uint256(keccak256(abi.encodePacked(hex"ff", address(this), bytes32(tokenL2Address), baseCodeHash))))
-        );
-    }
-
-    function toSplitUint(uint256 value) internal pure returns (uint256, uint256) {
-        uint256 low = value & ((1 << 128) - 1);
-        uint256 high = value >> 128;
-        return (low, high);
     }
 }
